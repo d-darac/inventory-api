b6a24d0ce4b5ddb4dfc56ca411172d777a77d22c
d-darac <dragicevic.dario@gmail.com>
2025-12-10 01:08:35 +0000

    add ApiConfig struct

---
+++ internal/api/api_config.go
@@ -0,0 +1,13 @@
+package api
+
+import "github.com/d-darac/inventory-assets/database"
+
+type ApiConfig struct {
+	Host       string
+	Port       int
+	DbURL      string
+	Db         *database.Queries
+	Platform   string
+	JWTSecret  string
+	MaxReqSize int
+}
