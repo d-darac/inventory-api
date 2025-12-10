2ecee4f2fd2d979ae060088255ae5bd6bb8fb043
d-darac <dragicevic.dario@gmail.com>
2025-12-10 01:05:44 +0000

    add ErrResponse struct add ListResponse struct add AccountResponse struct

---
+++ internal/api/response_types.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"time"
+
+	"github.com/d-darac/inventory-assets/database"
+	"github.com/google/uuid"
+)
+
+type ErrResponse struct {
+	Code    ErrorCode `json:"code,omitempty"`
+	Message string    `json:"message"`
+	Type    ErrorType `json:"type"`
+}
+
+type ListResponse struct {
+	Data    []interface{} `json:"data"`
+	HasMore bool          `json:"has_more"`
+	Url     string        `json:"url"`
+}
+
+type AccountResponse struct {
+	ID        uuid.UUID        `json:"id"`
+	CreatedAt time.Time        `json:"created_at"`
+	UpdatedAt time.Time        `json:"updated_at"`
+	Country   database.Country `json:"country"`
+	Nickname  string           `json:"nickname"`
+}
