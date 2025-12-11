9e864c441ad94e74107d16efb9efde4891f58e9d
d-darac <dragicevic.dario@gmail.com>
2025-12-11 17:49:52 +0000

    remove JWTSecret field remove MaxReqSize field

--- internal/api/api_config.go
+++ internal/api/api_config.go
@@ -3,11 +3,9 @@ package api
 import "github.com/d-darac/inventory-assets/database"
 
 type ApiConfig struct {
-	Host       string
-	Port       int
-	DbURL      string
-	Db         *database.Queries
-	Platform   string
-	JWTSecret  string
-	MaxReqSize int
+	Host     string
+	Port     int
+	DbURL    string
+	Db       *database.Queries
+	Platform string
 }
