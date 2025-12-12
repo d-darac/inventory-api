4e98546b01a1e37ed526ce872e07798c691ff490
d-darac <dragicevic.dario@gmail.com>
2025-12-12 00:59:12 +0000

    add util function GetIdFromPath

---
+++ internal/common/util.go
@@ -0,0 +1,16 @@
+package common
+
+import (
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+func GetIdFromPath(r *http.Request) (uuid.UUID, string) {
+	pathValue := r.PathValue("id")
+	id, err := uuid.Parse(pathValue)
+	if err != nil {
+		return uuid.UUID{}, InvalidIdMessage(pathValue, "account")
+	}
+	return id, ""
+}
