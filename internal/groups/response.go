d7bb2906f39f5aaaf9307167c8ef533cb350c2a1
d-darac <dragicevic.dario@gmail.com>
2025-12-16 00:04:09 +0000

    add groups/groupResponse

---
+++ internal/groups/response.go
@@ -0,0 +1,17 @@
+package groups
+
+import (
+	"time"
+
+	"github.com/d-darac/inventory-assets/str"
+	"github.com/google/uuid"
+)
+
+type groupResponse struct {
+	ID          uuid.UUID      `json:"id"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	Description str.NullString `json:"description"`
+	Name        string         `json:"name"`
+	ParentGroup uuid.NullUUID  `json:"parent_group"`
+}
