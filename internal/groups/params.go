dfaf99b8539d10075cf5c211bbaf7072dc58184b
d-darac <dragicevic.dario@gmail.com>
2025-12-16 15:11:17 +0000

    add listParams struct

--- internal/groups/params.go
+++ internal/groups/params.go
@@ -1,9 +1,34 @@
 package groups
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type createParams struct {
 	Description *string    `json:"description" validate:"lte=1024"`
 	Name        string     `json:"name" validate:"required,lte=64"`
 	ParentGroup *uuid.UUID `json:"parent_group"`
+}
+
+type listParams struct {
+	CreatedAt struct {
+		Gt  *time.Time `json:"gt"`
+		Lt  *time.Time `json:"lt"`
+		Gte *time.Time `json:"gte"`
+		Lte *time.Time `json:"lte"`
+	} `json:"created_at"`
+	UpdatedAt struct {
+		Gt  *time.Time `json:"gt"`
+		Lt  *time.Time `json:"lt"`
+		Gte *time.Time `json:"gte"`
+		Lte *time.Time `json:"lte"`
+	} `json:"updated_at"`
+	StartingAfter *uuid.UUID `json:"starting_after" validate:"excluded_with=EndingBefore"`
+	EndingBefore  *uuid.UUID `json:"ending_before" validate:"excluded_with=StartingAfter"`
+	ParentGroup   *uuid.UUID `json:"parent_group"`
+	Description   *string    `json:"description" validate:"lte=1024"`
+	Name          *string    `json:"name" validate:"lte=64"`
+	Limit         *int       `json:"limit" validate:"gt=0,lte=100"`
 }
