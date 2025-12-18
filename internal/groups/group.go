24f86f28ad2f5fbb9c2c5197fdc55f8b0522f9b6
d-darac <dragicevic.dario@gmail.com>
2025-12-18 23:39:43 +0400

    rename groupResponse struct to group and move it to group.go

--- internal/groups/group.go
+++ internal/groups/group.go
@@ -7,7 +7,7 @@ import (
 	"github.com/google/uuid"
 )
 
-type groupResponse struct {
+type group struct {
 	ID          uuid.UUID      `json:"id"`
 	CreatedAt   time.Time      `json:"created_at"`
 	UpdatedAt   time.Time      `json:"updated_at"`
